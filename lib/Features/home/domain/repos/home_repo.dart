21fab72cbd2b6cbe29230b90d46ead0509f33879
Ahmed Gamal Elnemr <ahmedelnemrtiger592@gmail.com>
2025-09-12 17:07:38 +0300

    fetch featured books use case

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,7 +1,8 @@
-
 import 'package:clean_architecture_course/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_architecture_course/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:clean_architecture_course/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_architecture_course/Features/home/domain/repos/home_repo.dart';
+import 'package:clean_architecture_course/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
