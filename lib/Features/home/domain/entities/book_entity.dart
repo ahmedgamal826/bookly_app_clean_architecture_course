1433c872ee1f7f4f2f031ede02702c077997b343
Ahmed Gamal Elnemr <ahmedelnemrtiger592@gmail.com>
2025-09-13 16:29:11 +0300

    create home remote data source

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,17 +1,19 @@
 class BookEntity {
-  final String image;
+  final String bookId;
+  final String? image;
   final String title;
-  final String authorName;
-  final num price;
-  final num rating;
-  final String? imageLink;
+  final String? authorName;
+  final num? price;
+  final num? rating;
+
 
   BookEntity({
+    required this.bookId,
     required this.image,
     required this.title,
     required this.authorName,
     required this.price,
     required this.rating,
-    this.imageLink,
+   
   });
 }
