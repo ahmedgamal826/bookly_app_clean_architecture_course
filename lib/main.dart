61d2224fe6f383af0eefe595078b1afd82d61495
Ahmed Gamal Elnemr <ahmedelnemrtiger592@gmail.com>
2025-09-14 16:26:49 +0300

    implement fetch newest books data

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
   runApp(
     const ProviderScope(
       child: Bookly(),
     ),
   );
-
-  Hive.registerAdapter(BookEntityAdapter());
-  Hive.openBox(kFeaturedBox);
 }
 
 class Bookly extends StatelessWidget {
