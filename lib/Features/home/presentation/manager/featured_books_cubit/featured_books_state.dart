5ff68bf1267cb2fa6f463ee61b037faa3efbe354
Ahmed Gamal Elnemr <ahmedelnemrtiger592@gmail.com>
2025-09-18 16:20:29 +0300

    create featured books states

---
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,18 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+sealed class FeaturedBooksState {}
+
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+  FeaturedBooksSuccess({required this.books});
+}
+
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String message;
+  FeaturedBooksFailure({required this.message});
+}
